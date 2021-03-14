4c7602007742bb0fa9891ceb4cd294679f9d4e71
Karen <kpl0813@yahoo.com>
2021-03-13 20:55:18 -0600

    integrated firebase into application and added sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCfe2ewjrSYbNnc61i82KdtaYuhsEfEizI",
+    authDomain: "crwn-clothing-db-6e9f6.firebaseapp.com",
+    projectId: "crwn-clothing-db-6e9f6",
+    storageBucket: "crwn-clothing-db-6e9f6.appspot.com",
+    messagingSenderId: "758998831279",
+    appId: "1:758998831279:web:ea2b48b38366420004445a",
+    measurementId: "G-5KQ4WSQ2DC"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GithubAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
