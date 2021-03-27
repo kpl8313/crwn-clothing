244e35e7d851f418ee842cbad6f11f216f0d3117
Karen <kpl0813@yahoo.com>
2021-03-27 17:41:12 -0500

    updated shop selector and data and added collection component to navigate to shop categories and display collection items

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,19 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
-export const selectShopCollections = createSelector(
+export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
 );
